5f961acd5742d30a2096426fd1e8932d599c4910
Melrose Ceballos <melroseceballos@Melroses-MBP.lan>
2023-03-06 01:03:38 -0800

    Finished creating schema and connection to Atlas

--- models/vampire.js
+++ models/vampire.js
@@ -2,7 +2,18 @@
 const mongoose = require('mongoose');
 
 const vampireSchema = new mongoose.Schema({
-  // add your code here to set up your schema
+  // requiring name
+    name: {type:String, required: true},
+    title: "Kill",
+    // Setting default hair color
+    hairColor: {type: String, default: "blonde"},
+    eyeColor: 'purple',
+    dob: new Date (1971, 2, 13, 7, 47),
+    loves: ['little kids','bikes'],
+    location: "Salem, Massachusetts, US ",
+    gender: 'non-binary',
+    // Setting minimum victim count to 1
+    victims: {type: Number, min: 1}
 });
 
 // Export your schema as a Monogoose model so that it can be accessed in `models/index.js`
